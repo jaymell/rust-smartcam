cbdc22f42d05b8314a4f29a11392c4e815b961e8
James Lucas <jamesmlucas@gmail.com>
2021-08-02 23:02:34 -0500

    update install script

--- install-open-cv.sh
+++ install-open-cv.sh
@@ -1,3 +1,5 @@
+sudo apt-get install git build-essential cmake qt5-default qtscript5-dev libssl-dev qttools5-dev qttools5-dev-tools qtmultimedia5-dev libqt5svg5-dev libqt5webkit5-dev libsdl2-dev libasound2 libxmu-dev libxi-dev freeglut3-dev libasound2-dev libjack-jackd2-dev libxrandr-dev libqt5xmlpatterns5-dev libqt5xmlpatterns5
+
 OPENCV_VERSION=4.5.2
 BUILD_FLAGS="
     -D BUILD_CUDA_STUBS=OFF
