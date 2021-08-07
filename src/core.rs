12e44faf0fe1564b4fb9878438e1915b4fb12951
James Lucas <jamesmlucas@gmail.com>
2021-08-06 22:51:42 -0500

    install script

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,8 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::{SystemTime};
+
+use chrono::{DateTime, Utc};
 use opencv::{
   prelude::*,
   Result,
@@ -7,10 +12,16 @@ use opencv::{
   imgproc::COLOR_BGR2GRAY,
   imgproc::cvt_color,
 };
-use std::sync::mpsc;
-use std::thread;
-use std::time::{SystemTime};
-use chrono::{DateTime, Utc};
+use ffmpeg_sys_next as ffs;
+use ffs::{
+  avpicture_fill,
+  AVCodecID::AV_CODEC_ID_RAWVIDEO
+};
+use ffmpeg_next::{
+  codec::encoder,
+  format::Pixel,
+  Picture
+};
 
 
 pub struct Frame {
@@ -58,6 +69,37 @@ impl Clone for Frame {
         height: self.height,
         width: self.width
       }
+    }
+
+    fn toAVFrame(&self) -> {
+
+
+        let mut dst = Picture::new(Pixel::YUYV422, self.width, self.height).unwrap();
+
+        codec::Codec encoder = codec::encoder::find(AV_CODEC_ID_RAWVIDEO);
+
+        // AVFormatContext* outContainer = avformat_alloc_context();
+        // how get output_file to be pipe instead:
+        let mut octx = format::output(&output_file).unwrap();
+
+        // AVStream *outStream = avformat_new_stream(outContainer, encoder);
+        let mut outStream = octx.new_stream(encoder);
+
+        // avcodec_get_context_defaults3(outStream->codec, encoder);
+        // outStream->codec->pix_fmt = AV_PIX_FMT_BGR24;
+        // outStream->codec->width = frame->cols;
+        // outStream->codec->height = frame->rows;
+
+        encoder.set_width(self.width);
+        encoder.set_height(self.height);
+        encoder.set_format(Pixel::YUYV422);
+
+        // avpicture_fill((AVPicture*)&dst, frame->data, AV_PIX_FMT_BGR24, outStream->codec->width, outStream->codec->height);
+        unsafe {
+          avpicture_fill(&dst, &frame.img, Pixel::YUYV422, self.width as c_int, self.height as c_int);
+        }
+
+        return dst;
     }
 
 }
