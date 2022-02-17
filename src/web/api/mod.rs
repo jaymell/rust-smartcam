01d3c08159db222e357dd5a4649fad2fab39d2bd
James Lucas <jamesmlucas@gmail.com>
2022-02-16 23:01:21 -0600

    fixup! compiles

--- src/web/api/mod.rs
+++ src/web/api/mod.rs
@@ -57,19 +57,14 @@ use webrtc::track::track_remote::TrackRemote;
 use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
 
 use tokio_stream::StreamExt;
-use futures::stream::Stream;
-
-use std::pin::Pin;
 
 #[get("/videos/<label>")]
 pub(crate) async fn get_videos_list(
     label: String,
     fs: &State<Arc<dyn VideoRepository + Send + Sync>>,
-// ) -> Json<Vec<VideoFile>> {
-) -> Option<String> {
-    let s: Vec<VideoFile> = fs.stream_files_by_label(label.clone()).await
-        .collect().await;
-    None
+) -> Json<Vec<VideoFile>> {
+    let s = fs.stream_files_by_label(label.clone()).await;
+    Json(s.collect().await)
 }
 
 #[get("/videos/<label>/<video>")]
